80e66fef6f4ebafa721872b5533231b9fb025c4c
Aleksey Alekseev <aleksey_alekseev@relex.ru>
2017-12-13 10:49:07 +0300

    Changed URL_WEBSITE

--- src/init.cpp
+++ src/init.cpp
@@ -507,7 +507,7 @@ std::string HelpMessage(HelpMessageMode mode)
 std::string LicenseInfo()
 {
     const std::string URL_SOURCE_CODE = "<https://github.com/litecoin-project/litecoin>";
-    const std::string URL_WEBSITE = "<https://litecoin.org>";
+    const std::string URL_WEBSITE = "<https://getlynx.io>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2011, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
