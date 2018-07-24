d5a678ce60a31a73af36bacfb28e7fa58a78938b
Aleksey Alekseev <aleksey_alekseev@relex.ru>
2018-07-24 11:37:56 +0300

    Added UnitTest for LynxRule2

--- src/test/lynx_rules_tests.cpp
+++ src/test/lynx_rules_tests.cpp
@@ -3,6 +3,8 @@
 #include "miner.h"
 #include "lynx_rules.h"
 #include "base58.h"
+#include "rpc/blockchain.h"
+#include "wallet/wallet.h"
 #include "test/test_bitcoin.h"
 #include <boost/test/unit_test.hpp>
 
@@ -28,6 +30,16 @@ static CBlock CreateBlock(const std::vector<CMutableTransaction>& txns, const CS
     return block;
 }
 
+CBlockIndex* GetDifficultyBlockIndex()
+{
+    const auto& consensusParams = Params().GetConsensus();
+    CBlockIndex* difficultyBlockIndex = chainActive.Tip();
+    for (int i = 0; i < consensusParams.HardFork5DifficultyPrevBlockCount; ++i)
+        difficultyBlockIndex = difficultyBlockIndex->pprev;
+    return difficultyBlockIndex;
+}
+
+
 BOOST_FIXTURE_TEST_SUITE(lynx_rules_tests, TestChain100Setup)
 
 BOOST_AUTO_TEST_CASE(lunx_rule1)
@@ -45,7 +57,7 @@ BOOST_AUTO_TEST_CASE(lunx_rule1)
         {address2, 0},
         {address3, 0}
     };
-
+    // Let's check that the rules are still inactive
     while (chainActive.Height() < consensusParams.HardFork4Height)
     {
         std::string errorString;
@@ -64,6 +76,7 @@ BOOST_AUTO_TEST_CASE(lunx_rule1)
     }
 
     {
+        // We used address, but did not use address2, address3
         std::string errorString;
         BOOST_CHECK(!IsValidAddressForMining(address, 0, chainActive.Tip(), consensusParams, errorString));
         BOOST_CHECK(IsValidAddressForMining(address2, 0, chainActive.Tip(), consensusParams, errorString));
@@ -102,6 +115,7 @@ BOOST_AUTO_TEST_CASE(lunx_rule1)
     }
 
     {
+        // We used address, address2, but did not use address3
         std::set<std::string> addressesProhibitedForMining;
         BOOST_CHECK(GetAddressesProhibitedForMining(chainActive.Tip(), consensusParams, addressesProhibitedForMining));
         auto etalonAddressesProhibitedForMining = std::set<std::string>{
@@ -132,6 +146,7 @@ BOOST_AUTO_TEST_CASE(lunx_rule1)
     }
 
     {
+        // We used all addresses, but address is again available
         std::set<std::string> addressesProhibitedForMining;
         BOOST_CHECK(GetAddressesProhibitedForMining(chainActive.Tip(), consensusParams, addressesProhibitedForMining));
         auto etalonAddressesProhibitedForMining = std::set<std::string>{
@@ -147,6 +162,79 @@ BOOST_AUTO_TEST_CASE(lunx_rule1)
         const CTxDestination* foundAddressForMining = FindAddressForMining(balances, chainActive.Tip(), consensusParams);
         BOOST_CHECK(foundAddressForMining != nullptr);
         BOOST_CHECK(*foundAddressForMining == address);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(lunx_rule2)
+{
+    const auto& consensusParams = Params().GetConsensus();
+    const CTxDestination address = coinbaseKey.GetPubKey().GetID();
+    const CTxDestination address2 = coinbaseKey2.GetPubKey().GetID();
+    const CTxDestination address3 = coinbaseKey3.GetPubKey().GetID();
+
+    const auto zeroBalances = std::map<CTxDestination, CAmount>{
+        {address, 0},
+        {address2, 0},
+        {address3, 0}
+    };
+    // Let's check that the rules are still inactive
+    while (chainActive.Height() < consensusParams.HardFork5Height)
+    {
+        BOOST_CHECK_EQUAL(GetMinBalanceForMining(chainActive.Tip(), consensusParams), 0);
+
+        const CTxDestination* addressForMining = FindAddressForMining(zeroBalances, chainActive.Tip(), consensusParams);
+        BOOST_CHECK(addressForMining != nullptr);
+
+        auto oldChainHeight = chainActive.Height();
+        CreateAndProcessBlock({}, GetScriptForDestination(*addressForMining));
+        BOOST_CHECK(chainActive.Height() > oldChainHeight);
+    }
+
+    // Let's check that the rule has turned on
+    BOOST_CHECK_EQUAL(FindAddressForMining(zeroBalances, chainActive.Tip(), consensusParams), nullptr);
+
+    // The difficulty is small, so a minimum limit is required for mining
+    BOOST_CHECK_EQUAL(GetMinBalanceForMining(chainActive.Tip(), consensusParams), consensusParams.HardFork5LowerLimitMinBalance);
+    {
+        CBlockIndex* difficultyBlockIndex = GetDifficultyBlockIndex();
+        auto savedNBits = difficultyBlockIndex->nBits;
+
+        // Usually the minimum balance is defined as pow(DiffcultyPredN, consensusParams.HardFork5CoinAgePow)
+        difficultyBlockIndex->nBits = 480000000;
+        CAmount etalonMinBalance = static_cast<CAmount>(std::pow(GetDifficulty(difficultyBlockIndex), consensusParams.HardFork5CoinAgePow)*COIN);
+        BOOST_CHECK_EQUAL(GetMinBalanceForMining(chainActive.Tip(), consensusParams), etalonMinBalance);
+
+        // The difficulty is too great, the necessary limit for mining will be limited from above
+        difficultyBlockIndex->nBits = 10000;
+        BOOST_CHECK_EQUAL(GetMinBalanceForMining(chainActive.Tip(), consensusParams), consensusParams.HardFork5UpperLimitMinBalance);
+
+        difficultyBlockIndex->nBits = savedNBits;
+    }
+
+    {
+        // At the coinbaseKey4 there is no money, the block should not be accepted
+        CKey coinbaseKey4;
+        coinbaseKey4.MakeNewKey(true);
+        auto oldChainHeight = chainActive.Height();
+        CreateAndProcessBlock({}, GetScriptForRawPubKey(coinbaseKey4.GetPubKey()));
+        BOOST_CHECK(chainActive.Height() == oldChainHeight);
+    }
+
+    {
+        // Creating a block with an address with enough coins
+        std::map<CTxDestination, CAmount> balances = {
+            {address, pcoinsTip->GetAddressBalance(CBitcoinAddress(address).ToString())},
+            {address2, pcoinsTip->GetAddressBalance(CBitcoinAddress(address2).ToString())},
+            {address3, pcoinsTip->GetAddressBalance(CBitcoinAddress(address2).ToString())}
+        };
+
+        const CTxDestination* addressForMining = FindAddressForMining(balances, chainActive.Tip(), consensusParams);
+        BOOST_CHECK(addressForMining != nullptr);
+        BOOST_CHECK(balances[*addressForMining] >= GetMinBalanceForMining(chainActive.Tip(), consensusParams));
+
+        auto oldChainHeight = chainActive.Height();
+        CreateAndProcessBlock({}, GetScriptForDestination(*addressForMining));
+        BOOST_CHECK(chainActive.Height() == oldChainHeight + 1);
     }
 }
 
